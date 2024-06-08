3caf46f71c37489b39a321864694c302e88fea79
Jakub <jakub.bchyla@gmail.com>
2024-06-08 09:25:36 +0200

    added toolbar componanet

---
+++ Coach-FE/src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss'
+})
+export class ToolbarComponent {
+
+}
