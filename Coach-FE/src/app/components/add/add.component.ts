381ae0942ae3fa6d3364d7315e606766fb8b6d70
Jakub <jakub.bchyla@gmail.com>
2024-07-15 17:54:52 +0200

    center add card, add devider

--- Coach-FE/src/app/components/add/add.component.ts
+++ Coach-FE/src/app/components/add/add.component.ts
@@ -7,6 +7,8 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatCardModule} from "@angular/material/card";
+import {MatDividerModule} from "@angular/material/divider";
 
 @Component({
   selector: 'app-add',
@@ -16,7 +18,9 @@ import {MatFormFieldModule} from "@angular/material/form-field";
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule
   ],
   templateUrl: './add.component.html',
   styleUrl: './add.component.scss',
