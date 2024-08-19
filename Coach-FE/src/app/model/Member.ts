a7039cfa60aad28c723acff6562ed41812489b84
Jakub <jakub.bchyla@gmail.com>
2024-08-19 18:09:09 +0200

    added goal

--- Coach-FE/src/app/model/Member.ts
+++ Coach-FE/src/app/model/Member.ts
@@ -1,3 +1,5 @@
+import {Goal} from "./Goal";
+
 export class Member {
   id?: number;
   firstName?: string;
@@ -5,4 +7,5 @@ export class Member {
   email?: string;
   phone?: number;
   birthday?: string;
+  goal?: Goal;
 }
