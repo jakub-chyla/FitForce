26d54e27993db6f7ee12c974cc3e5e6204071f0a
Jakub <jakub.bchyla@gmail.com>
2024-09-01 20:01:07 +0200

    added select setval('members_seq', 1);

--- Coach-BE/coach/src/main/resources/db/migration/V1.0.0__create_members_table.sql
+++ Coach-BE/coach/src/main/resources/db/migration/V1.0.0__create_members_table.sql
@@ -1,13 +1,17 @@
--- Create sequence for goals
+
 create sequence goals_seq increment by 1;
 
--- Create goals table
+select setval('goals_seq', 1);
+
 create table goals (
     id BIGINT PRIMARY KEY,
     value TEXT UNIQUE NOT NULL
 );
 
--- Create members table with foreign key to goals table
+create sequence members_seq increment by 1;
+
+select setval('members_seq', 1);
+
 create table members (
     id BIGINT PRIMARY KEY,
     first_name TEXT,
@@ -21,12 +25,10 @@ create table members (
     CONSTRAINT fk_goal FOREIGN KEY (goal_id) REFERENCES goals(id)
 );
 
--- Insert values into goals table
 insert into goals (id, value) values
   (1, 'Lose weight'),
   (2, 'Gain muscles');
 
--- Insert values into members table
 insert into members (id, first_name, last_name, phone, email, birthday, goal_id, is_active, created)
 values
   (1, 'John', 'Williams', 1234567890, 'john.williams@example.com', '1985-05-15', 1, true, CURRENT_TIMESTAMP);
