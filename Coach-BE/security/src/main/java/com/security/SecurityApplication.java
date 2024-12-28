2bc027efd8d5b64406651463bff32da66c210171
jakub-chyla <jakub.bchyla@gmail.com>
2024-12-28 14:52:37 +0100

    added emial

--- Coach-BE/security/src/main/java/com/security/SecurityApplication.java
+++ Coach-BE/security/src/main/java/com/security/SecurityApplication.java
@@ -1,15 +1,30 @@
 package com.security;
 
+import com.security.service.EmailSenderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-	}
+    @Autowired
+    private EmailSenderService emailSenderService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void sendMail() {
+        emailSenderService.sendEmail("jakub.bchyla@gmail.com",
+                "my subject",
+                "this is body");
+    }
+
 
 }
